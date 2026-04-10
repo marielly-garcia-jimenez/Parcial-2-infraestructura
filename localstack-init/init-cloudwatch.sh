04b51c017ed6576616af3840fb45a0d1710624ca
marielly-garcia-jimenez <131173711+marielly-garcia-jimenez@users.noreply.github.com>
2026-04-09 22:26:27 -0600

    segundo commit

--- localstack-init/init-cloudwatch.sh
+++ localstack-init/init-cloudwatch.sh
@@ -3,3 +3,4 @@ awslocal logs create-log-group --log-group-name producto-log-group
 awslocal logs create-log-group --log-group-name ordenes-log-group
 awslocal logs create-log-group --log-group-name pagos-log-group
 awslocal logs create-log-group --log-group-name api-gateway-log-group
+awslocal logs create-log-group --log-group-name broker-log-group
